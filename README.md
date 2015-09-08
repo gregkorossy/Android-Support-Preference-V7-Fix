ce6c56c47f591c018afa73d7c9f6a34271df9001
Gergely Kőrössy <korossyg@gmail.com>
2015-09-08 15:12:51 +0200

    Added interesting case of dynamic preference creation using the material styled context

--- README.md
+++ README.md
@@ -42,6 +42,20 @@ This also means that even though you are using only v7, you have to include the 
 
 *Note that I did not test this background-fixer solution extensively so it might mess up other parts of your app. This is just a temporary (i.e. experimental) bugfix until Google releases either a less buggy version of the lib or the source code so we could fix it.*
 
+# Interesting things
+These are not considered bugs but they can give you a headache.
+
+### Creating Preference programmatically
+When you create a `Preference`, you have to pass a context to its constructor. When you pass context by calling `getActivity()` or `getContext()` on your `PreferenceFragmentCompat`, you give the newly created `Preference` a context which is not styled with your `preferenceTheme` but your Activity's main theme. To pass the right context object, you have to do this:
+
+```java
+Context ctx = getPreferenceManager().getContext();
+
+Preference preference = new Preference(ctx);
+// setup your preference and add it to a category or the preference screen
+```
+And voilà, now your `preference` instance is material styled (*or whatever style you set as your `preferenceTheme`'s `preferenceStyle`*).
+
 # Android-Support-Preference-V7-Fix
 ~~Android preference-v7 support library doesn't contain material design layout files so the preferences screen looks bad on API 21+. This is a temporary fix until Google fixes it.~~
 
--- app/src/main/java/com/takisoft/preferencefix/MyPreferenceFragment.java
+++ app/src/main/java/com/takisoft/preferencefix/MyPreferenceFragment.java
@@ -1,7 +1,9 @@
 package com.takisoft.preferencefix;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.preference.EditTextPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
 /**
@@ -12,5 +14,29 @@ public class MyPreferenceFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.settings);
+
+        testDynamicPrefs();
+    }
+
+    private void testDynamicPrefs() {
+        final Context ctx = getPreferenceManager().getContext(); // this is the material styled context
+
+        final PreferenceCategory dynamicCategory = (PreferenceCategory) findPreference("pref_categ");
+
+        Preference prefAdd = findPreference("pref_add");
+        prefAdd.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            private int n = 0;
+
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Preference newPreference = new Preference(ctx);
+
+                newPreference.setTitle("New preference " + n++);
+                newPreference.setSummary(Long.toString(System.currentTimeMillis()));
+
+                dynamicCategory.addPreference(newPreference);
+                return true;
+            }
+        });
     }
 }
