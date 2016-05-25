71a34431f73d87e4c12bd87200b32e8cf50f97f3
Gergely Kőrössy <korossyg@gmail.com>
2016-05-25 22:11:43 +0200

    SwitchPreferenceCompat test

---
+++ preference-v7/src/main/java/com/takisoft/fix/support/v7/preference/SwitchPreferenceCompat.java
@@ -0,0 +1,81 @@
+package com.takisoft.fix.support.v7.preference;
+
+import android.content.Context;
+import android.support.v7.preference.PreferenceViewHolder;
+import android.support.v7.preference.PreferenceViewHolderFix;
+import android.support.v7.widget.SwitchCompat;
+import android.util.AttributeSet;
+
+public class SwitchPreferenceCompat extends android.support.v7.preference.SwitchPreferenceCompat {
+    //private boolean mCheckedSet;
+
+    public SwitchPreferenceCompat(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public SwitchPreferenceCompat(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public SwitchPreferenceCompat(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SwitchPreferenceCompat(Context context) {
+        super(context);
+    }
+
+    SwitchCompat switchCompat;
+
+    /*@Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        switchCompat = (SwitchCompat) holder.findViewById(R.id.switchWidget);
+        Log.d("SwitchCompat", "" + switchCompat);
+    }*/
+
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(new PreferenceViewHolderFix(holder));
+        //this.syncSwitchView(switchView);
+        switchCompat = (SwitchCompat) holder.findViewById(R.id.switchWidget);
+        /*if (switchCompat != null) {
+            (switchCompat).setChecked(this.mChecked);
+        }*/
+        //this.syncSummaryView(holder);
+    }
+
+    @Override
+    public void setChecked(boolean checked) {
+        super.setChecked(checked);
+
+        /*boolean changed = this.mChecked != checked;
+        if (changed || !this.mCheckedSet) {
+            this.mChecked = checked;
+            this.mCheckedSet = true;
+            this.persistBoolean(checked);
+            if (changed) {
+                this.notifyDependencyChange(this.shouldDisableDependents());
+                this.notifyChanged();
+            }
+        }*/
+
+        /*Log.d("SwitchPreferenceCompat", "setChecked: " + checked);
+        Log.d("SwitchPreferenceCompat", "windowToken: " + switchCompat.getWindowToken());
+        Log.d("SwitchPreferenceCompat", "isLaidOut: " + ViewCompat.isLaidOut(switchCompat));
+        Log.d("SwitchPreferenceCompat", "isShown: " + switchCompat.isShown());*/
+
+        switchCompat.setChecked(checked);
+
+
+
+        /*if (switchCompat.getWindowToken() != null && ViewCompat.isLaidOut(switchCompat) && switchCompat.isShown()) {
+            try {
+                Method animateThumbToCheckedState = SwitchCompat.class.getDeclaredMethod("animateThumbToCheckedState", Boolean.TYPE);
+                animateThumbToCheckedState.setAccessible(true);
+                animateThumbToCheckedState.invoke(switchCompat, checked);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }*/
+    }
+}
